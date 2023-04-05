c99b1045bb2ecdf165114317188e1607ce4ea71e
psbyd <null>
2023-04-05 19:08:45 -0400

    thing

--- tetris/tests/tests.rs
+++ tetris/tests/tests.rs
@@ -1,17 +1,8 @@
 #[cfg(test)]
-mod good_get_squares {
+mod test_clear {
     use super::*;
     #[test]
-    fn good_get_squares() -> () {
-        let board = [[0; 10]; 40];  
-        let mut bag = Bag::new();
-        let piece =  ActivePiece {
-            tetromino: Tetromino::I,
-            origin: Pos(0,1),
-            rotation: State::Right,
-        };
-
-        let ans = [Pos(0,0),Pos(0,1),Pos(0,2),Pos(0,3)];
-        assert!(piece.get_squares() == ans);
+    fn test_clear() -> () {
+        let t = tetris::new();
     }
 }
