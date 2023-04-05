ab473b48bd6eed9e364ff398c7e00bc29b953fa9
Eric Hamilton <ericjean873@gmail.com>
2023-04-05 18:31:42 -0400

    implemented rudimentary "debug" test mode for main

--- tetroxide/src/main.rs
+++ tetroxide/src/main.rs
@@ -1,13 +1,52 @@
+use std::io::{self, BufRead};
 use std::process;
+use tetris::tetris::Tetris;
 use tetroxide::tetroxide::Game;
 
 fn main() -> Result<(), std::io::Error> {
-    let mut tet = Game::new();
-    match tet.run() {
-        Ok(_) => Ok(()),
-        Err(err) => {
-            eprintln!("Unexpected error occurred: {}", err);
-            process::exit(1);
+    let mut tet = Tetris::new();
+    // We can rotate and not drop for a limited amount of time in
+    // debug mode.
+    let mut frame_count = 0;
+    const MAX_FRAMES: usize = 3;
+    println!("{}", tet);
+    while !tet.is_game_over {
+        println!("Input: ");
+        let mut buffer = String::new();
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        handle.read_line(&mut buffer)?;
+        let lower = buffer.to_lowercase();
+        match lower.trim() {
+            "w" => tet.hold(),
+            "q" | "e" => {
+                if lower.trim() == "q" {
+                    tet.rotate(true);
+                } else {
+                    tet.rotate(false)
+                }
+                if frame_count < MAX_FRAMES {
+                    frame_count += 1;
+                    println!("{}", tet);
+                    continue;
+                }
+                frame_count = 0;
+            }
+            "a" => tet.shift(true),
+            "d" => tet.shift(false),
+            "s" | _ => {
+                if lower.trim() == "s" {
+                    tet.hard_drop();
+                } else {
+                    tet.drop();
+                }
+                tet.clear_lines();
+                println!("{}", tet);
+                continue;
+            }
         }
+        tet.drop();
+        println!("{}", tet);
     }
+    Ok(())
 }
