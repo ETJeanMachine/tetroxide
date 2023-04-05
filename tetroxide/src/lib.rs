ac7b002d388c24f8a57b05233a941a465655b9b6
Eric Hamilton <ericjean873@gmail.com>
2023-04-05 13:52:29 -0400

    added some stuff for key events

--- tetroxide/src/lib.rs
+++ tetroxide/src/lib.rs
@@ -1,16 +1,14 @@
 pub mod tetroxide {
-    use tetris::tetris::Tetris;
-    use spin_sleep::LoopHelper;
     use crossterm::{
         cursor::{Hide, Show},
         event::{read, EnableMouseCapture, Event, KeyCode, KeyEvent},
         execute,
+        terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
         Result,
-        terminal::{
-            disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
-        },
     };
+    use spin_sleep::LoopHelper;
     use std::io::{stdout, Write};
+    use tetris::tetris::Tetris;
     use tui_input::backend::crossterm as backend;
     use tui_input::backend::crossterm::EventHandler;
     use tui_input::Input;
@@ -22,11 +20,11 @@ pub mod tetroxide {
         Drop,
         Left,
         Right,
-    }   
+    }
 
     pub struct Game {
         tetris: Tetris,
-        looper: LoopHelper
+        looper: LoopHelper,
     }
     impl Game {
         pub fn new() -> Self {
@@ -34,8 +32,8 @@ pub mod tetroxide {
                 tetris: Tetris::new(),
                 looper: LoopHelper::builder()
                     .report_interval_s(0.5)
-                    .build_with_target_rate(60)
-            } 
+                    .build_with_target_rate(60),
+            }
         }
 
         pub fn run(&mut self) -> Result<()> {
@@ -44,22 +42,34 @@ pub mod tetroxide {
             let mut stdout = stdout.lock();
 
             execute!(stdout, Hide, EnterAlternateScreen, EnableMouseCapture)?;
-
-            let mut input: Input = "Hello ".into();
-            backend::write(&mut stdout, input.value(), input.cursor(), (0, 0), 15)?;
+            
+            self.tetris.drop();
+            self.tetris.drop();
+            println!("{}", self.tetris);
+            let input = format!("{}", self.tetris);
+            backend::write(&mut stdout, input.as_str(), 90, (0, 0), 15)?;
             stdout.flush()?;
 
             let mut frame_count: i64 = 1;
 
-            loop {
+            while !self.tetris.is_game_over {
                 self.looper.loop_start();
-                
+
                 let event = read()?;
+
+                
 
                 match event {
                     Event::Key(KeyEvent { code, .. }) => match code {
                         KeyCode::Esc => break,
-                        _ => {}
+                        KeyCode::Left => todo!(),
+                        KeyCode::Right => todo!(),
+                        KeyCode::Up => todo!(),
+                        KeyCode::Down => todo!(),
+                        KeyCode::Char(_) => todo!(),
+                        KeyCode::F(1) => todo!(),
+                        KeyCode::Modifier(_) => todo!(),
+                        _ => {},
                     },
                     _ => {}
                 }
@@ -72,6 +82,7 @@ pub mod tetroxide {
                 self.looper.loop_sleep();
                 frame_count += 1;
             }
+            stdout.flush()?;
 
             Ok(())
         }
