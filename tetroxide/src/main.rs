8a20c9b0c654214784bfeee13090d7212fed5c94
Eric Hamilton <ericjean873@gmail.com>
2023-04-06 09:57:15 -0400

    added debug command line arg flag

--- tetroxide/src/main.rs
+++ tetroxide/src/main.rs
@@ -1,54 +1,67 @@
+use clap::Parser;
 use std::io::{self, BufRead};
-use std::process;
 use tetris::tetris::Tetris;
 use tetroxide::tetroxide::Game;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = false)]
+    debug: bool,
+}
+
 fn main() -> Result<(), std::io::Error> {
-    print!("\x1B[2J\x1B[1;1H");
-    let mut tet = Tetris::default();
-    // We can rotate and not drop for a limited amount of time in
-    // debug mode.
-    let mut frame_count = 0;
-    const MAX_FRAMES: usize = 3;
-    println!("{}", tet);
-    while !tet.is_game_over {
-        println!("Input: (w - hold | q & e - rotate | a & d - shift | s - hard drop | enter - soft drop)");
-        let mut buffer = String::new();
-        let stdin = io::stdin();
-        let mut handle = stdin.lock();
-        handle.read_line(&mut buffer)?;
+    let args = Args::parse();
+    if args.debug {
         print!("\x1B[2J\x1B[1;1H");
-        let lower = buffer.to_lowercase();
-        match lower.trim() {
-            "w" => tet.hold(),
-            "q" | "e" => {
-                if lower.trim() == "q" {
-                    tet.rotate(true);
-                } else {
-                    tet.rotate(false)
+        let mut tet = Tetris::default();
+        // We can rotate and not drop for a limited amount of time in
+        // debug mode.
+        let mut frame_count = 0;
+        const MAX_FRAMES: usize = 3;
+        println!("{}", tet);
+        while !tet.is_game_over {
+            println!("Input: (w - hold | q & e - rotate | a & d - shift | s - hard drop | enter - soft drop)");
+            let mut buffer = String::new();
+            let stdin = io::stdin();
+            let mut handle = stdin.lock();
+            handle.read_line(&mut buffer)?;
+            print!("\x1B[2J\x1B[1;1H");
+            let lower = buffer.to_lowercase();
+            match lower.trim() {
+                "w" => tet.hold(),
+                "q" | "e" => {
+                    if lower.trim() == "q" {
+                        tet.rotate(true);
+                    } else {
+                        tet.rotate(false)
+                    }
+                    if frame_count < MAX_FRAMES {
+                        frame_count += 1;
+                        println!("{}", tet);
+                        continue;
+                    }
+                    frame_count = 0;
                 }
-                if frame_count < MAX_FRAMES {
-                    frame_count += 1;
+                "a" => tet.shift(true),
+                "d" => tet.shift(false),
+                "s" | _ => {
+                    if lower.trim() == "s" {
+                        tet.hard_drop();
+                    } else {
+                        tet.drop();
+                    }
+                    tet.clear_lines();
                     println!("{}", tet);
                     continue;
                 }
-                frame_count = 0;
             }
-            "a" => tet.shift(true),
-            "d" => tet.shift(false),
-            "s" | _ => {
-                if lower.trim() == "s" {
-                    tet.hard_drop();
-                } else {
-                    tet.drop();
-                }
-                tet.clear_lines();
-                println!("{}", tet);
-                continue;
-            }
+            tet.drop();
+            println!("{}", tet);
         }
-        tet.drop();
-        println!("{}", tet);
+    } else {
+        let mut game = Game::new();
+        game.run()?;
     }
     Ok(())
 }
